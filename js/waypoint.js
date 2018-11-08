fa1ebbae8922a2261e2aa77f07f58263f4bd7bfc
N.TAHAR <najibtaharberrabah@gmail.com>
2018-11-08 22:06:44 +0100

    test

--- js/waypoint.js
+++ js/waypoint.js
@@ -1,2 +1,15 @@
+var ids = ['trends', 'news'];
 
 
+ids.forEach(function(id) {
+  var element = document.getElementById(id);
+  console.log(element);
+  var waypoint = new Waypoint({
+    element: element,
+    handler: function() {
+      console.log('reached')
+      element.classList.add('reset')
+    },
+    offset: 250
+  });
+});
